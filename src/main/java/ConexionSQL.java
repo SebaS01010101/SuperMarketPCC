6347c89b5f80a290e5a6366af3c65170dd9ff4e4
BerAxz <bastiaxd@gmail.com>
2024-06-23 22:45:41 -0400

    Update ConexionSQL.java
    Borre metodos innecesarios y añadi los nuevos

--- src/main/java/ConexionSQL.java
+++ src/main/java/ConexionSQL.java
@@ -1,7 +1,5 @@
 import java.sql.*;
 
-import static java.sql.DriverManager.getConnection;
-
 public class ConexionSQL {
     private Connection connection;
     private final static String host = "jdbc:mysql://localhost/supermercado_inventario";
@@ -10,7 +8,7 @@ public class ConexionSQL {
 
     public void conexionBD() {
         try {
-            connection = getConnection(host, user, password);
+            connection = DriverManager.getConnection(host, user, password);
             System.out.println("Conexión establecida");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -18,52 +16,20 @@ public class ConexionSQL {
         }
     }
 
-    public void createTableProductos() {
-        try (Statement statement = connection.createStatement()) {
-            String consult = "CREATE TABLE IF NOT EXISTS productos (" +
-                    "id INT AUTO_INCREMENT PRIMARY KEY, " +
-                    "nombre VARCHAR(50) NOT NULL, " +
-                    "precio DECIMAL(10, 2) NOT NULL, " +
-                    "cantidad INT NOT NULL)";
-            statement.executeUpdate(consult);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-
-        }
+    public Connection getConnection() {
+        return connection;
     }
 
-    public void createProduct(String nombre, java.math.BigDecimal precio, int cantidad) {
-        try {
-            String consult = "INSERT INTO productos (nombre, precio, cantidad) VALUES (?, ?, ?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(consult)) {
-                preparedStatement.setString(1, nombre);
-                preparedStatement.setBigDecimal(2, precio);
-                preparedStatement.setInt(3, cantidad);
-                preparedStatement.executeUpdate();
-                System.out.println("Producto añadido");
+    public void cerrarConexion(){
+        try{
+            if (connection != null && !connection.isClosed()){
+                connection.close();
+                System.out.println("Conexión cerrada");
             }
-        } catch (SQLException e) {
+        } catch (SQLException e){
             System.out.println(e.getMessage());
-
+            throw new RuntimeException("No se ha podido cerrar la conexión");
         }
     }
 
-    public void displayProducts() {
-        try {
-            String consult = "SELECT * FROM productos";
-            try (Statement statement = connection.createStatement()) {
-                ResultSet resultSet = statement.executeQuery(consult);
-                while (resultSet.next()) {
-                    System.out.println("ID: " + resultSet.getInt("id"));
-                    System.out.println("Nombre: " + resultSet.getString("nombre"));
-                    System.out.println("Precio: " + resultSet.getBigDecimal("precio"));
-                    System.out.println("Cantidad: " + resultSet.getInt("cantidad"));
-                    System.out.println();
-                }
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-
-        }
-    }
 }
