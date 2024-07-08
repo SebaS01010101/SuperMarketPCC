6c954d86879891e647c2881a97a37ae9364fd5e8
SebaS01010101 <s.sandoval18@ufromail.cl>
2024-07-07 21:59:18 -0400

    a

--- src/main/java/supermarketpcc/logica/Main.java
+++ src/main/java/supermarketpcc/logica/Main.java
@@ -1,21 +1,11 @@
 package supermarketpcc.logica;
-import Servlets.SvProducto;
 
 import java.sql.SQLException;
-import java.util.List;
 
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        SvProducto svProducto = new SvProducto();
-        DeleteProdutct deleteProdutct = new DeleteProdutct();
-        List<Producto> productos = svProducto.obtenerProductos();
-        for (Producto producto : productos) {
-            System.out.println(producto);
-        }
-        deleteProdutct.eliminarPorID(1);
 
-
-
+    }
 }
